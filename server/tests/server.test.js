397a85e97da28924e5d8bbb09b07f2f2ae56ed69
Calvin Lin <cl3086@nyu.edu>
2017-08-18 02:40:14 -0400

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,12 +5,21 @@ const request = require("supertest");
 const {app} = require("./../server");
 const {Todo} = require("./../models/todo");
 
+const todos = [{
+    text: "first test todo"
+}, {
+    text: "second test todo"
+}];
+
 // we are assuming the database to be empty. This might not be the case
 // beforeEach will run before each test case
 beforeEach((done) => {
     // .remove is a mongodb native function
     // this will wipe the database
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        // insertMany will take an array and insert all elements as documents
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe("POST /todos", () => {
@@ -34,7 +43,7 @@ describe("POST /todos", () => {
                     return done(err);
                 }
                 // .find() is a mongodb native function
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -53,9 +62,21 @@ describe("POST /todos", () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((err) => done(err));
             });
+    });
+});
+
+describe("GET /todos", () => {
+    it("should get all todos", (done) => {
+        request(app)
+            .get("/todos")
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
     });
 });
